73610743ea1269e6c421ec6aecebb94d4ee665b2
sm5duck <sm5duck5@gmail.com>
2017-06-01 21:31:51 +0900

    모니터 색상 변화 시 효과음 추가

--- BlinkBlink_EyeJoah/Main Controls/Control1_home.cs
+++ BlinkBlink_EyeJoah/Main Controls/Control1_home.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using BlinkBlink_EyeJoah.Chart.ConstantChange;
 using System.Threading;
+using System.IO;
+using System.Reflection;
+using System.Media;
 
 namespace BlinkBlink_EyeJoah
 {
@@ -42,12 +45,18 @@ namespace BlinkBlink_EyeJoah
             get { return con; }
             set { con = value; }
         }
-
+        
         private void BlinkTimer_Tick(object sender, EventArgs e)
         {
             convertOriginalColor = true;
 
             colorChange.changeScreenColor("color");
+
+            string pathToSound = Assembly.GetExecutingAssembly().Location; // returns the full path to Application.exe is
+            pathToSound = Path.GetDirectoryName(pathToSound); // returns the directory where Application.exe (and file.wav) are. 
+            pathToSound = Path.Combine(pathToSound, "facebook_sound.wav"); // combines the directory name with file.wav. 
+            SoundPlayer sound = new SoundPlayer(pathToSound); // loads the file. 
+            sound.Play(); // plays the file. 
 
             //Thread.Sleep(3000);
 
