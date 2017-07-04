e696b5aa7d561cdb2652a00c1a1b23074b5fad43
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-07-04 21:18:35 +0900

    Facebook API 추가 및 기본 Facebook Login에 필요한 기본 Class 생성

---
+++ BlinkBlink_EyeJoah/FacebookLogin/Analyze.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlinkBlink_EyeJoah.FacebookLogin
+{
+    public partial class Analyze : Form
+    {
+        public Analyze()
+        {
+            InitializeComponent();
+        }
+    }
+}
